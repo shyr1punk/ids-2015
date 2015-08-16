974325bf2ae2fa0cc4f11d0bb4013c8cee0f85a1
shyr1punk <shyr1punk@gmail.com>
2015-08-16 18:41:49 +0300

    Add comments and JSDoc

--- player/js/app.js
+++ player/js/app.js
@@ -4,12 +4,20 @@
     var AudioPlayer = {
         audioContext: null,
         bufferSource: null,
+        /**
+         * Create source buffer from file buffer
+         *
+         * @param buffer
+         */
         createBufferSourse: function (buffer) {
             this.bufferSource = this.audioContext.createBufferSource();
             this.bufferSource.loop = true;
             this.bufferSource.connect(this.audioContext.destination);
             AudioPlayer.bufferSource.buffer = buffer;
         },
+        /**
+         * Play action
+         */
         play: function () {
             /**
              * Recreate Buffer Source
@@ -19,14 +27,23 @@
             this.createBufferSourse(this.bufferSource.buffer);
             this.bufferSource.start(0);
         },
+        /**
+         * Stop action
+         */
         stop: function () {
             this.bufferSource.stop();
         },
+        /**
+         * Pause action
+         */
         pause: function () {
             this.bufferSource.stop();
         }
     };
 
+    /**
+     * When page is loaded initialize WebAudioAPI
+     */
     window.addEventListener('load', function init() {
         try {
             window.AudioContext = window.AudioContext;
@@ -37,6 +54,9 @@
         }
     }, false);
 
+    /**
+     * Load file from input as array buffer
+     */
     document.getElementById('input').addEventListener('change', function handleFiles() {
         var reader = new FileReader();
         reader.onload = function (ev) {
@@ -49,14 +69,23 @@
         document.getElementById('filename').innerHTML = this.files[0].name;
     }, false);
 
+    /**
+     * Handle click Play button
+     */
     document.getElementById('play').addEventListener('click', function handlePlay() {
         AudioPlayer.play();
     }, false);
 
+    /**
+     * Handle click Pause button
+     */
     document.getElementById('pause').addEventListener('click', function handlePause() {
         AudioPlayer.pause();
     }, false);
 
+    /**
+     * Handle click Stop button
+     */
     document.getElementById('stop').addEventListener('click', function handleStop() {
         AudioPlayer.stop();
     }, false);
