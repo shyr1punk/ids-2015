6dec44b904eec86ebefb8b5baf813b0a618d949d
shyr1punk <shyr1punk@gmail.com>
2015-08-16 17:52:20 +0300

    Refactor: loading file and add AudioPlayer object

--- player/js/app.js
+++ player/js/app.js
@@ -1,31 +1,62 @@
 (function () {
     'use strict';
-    var inputElement = document.getElementById('input');
-    var context;
-    var audio_player;
-    window.addEventListener('load', init, false);
-    function init() {
+
+    var AudioPlayer = {
+        audioContext: null,
+        bufferSource: null,
+        createBufferSourse: function (buffer) {
+            this.bufferSource = this.audioContext.createBufferSource();
+            this.bufferSource.loop = true;
+            this.bufferSource.connect(this.audioContext.destination);
+            AudioPlayer.bufferSource.buffer = buffer;
+        },
+        play: function () {
+            /**
+             * Recreate Buffer Source
+             *
+             * @see http://stackoverflow.com/questions/9439585/play-an-audiobuffersourcenode-twice
+             */
+            this.createBufferSourse(this.bufferSource.buffer);
+            this.bufferSource.start(0);
+        },
+        stop: function () {
+            this.bufferSource.stop();
+        },
+        pause: function () {
+            this.bufferSource.stop();
+        }
+    };
+
+    window.addEventListener('load', function init() {
         try {
             window.AudioContext = window.AudioContext;
-            context = new AudioContext();
+            AudioPlayer.audioContext = new AudioContext();
             console.info('Web Audio API AudioContext created successfully');
         } catch (e) {
             alert('Web Audio API is not supported in this browser');
         }
-    }
-    inputElement.addEventListener('change', handleFiles, false);
-    document.getElementById('play').addEventListener('click', function () {
-        audio_player.play();
     }, false);
-    document.getElementById('pause').addEventListener('click', function () {
-        audio_player.pause();
+
+    document.getElementById('input').addEventListener('change', function handleFiles() {
+        var reader = new FileReader();
+        reader.onload = function (ev) {
+            AudioPlayer.audioContext.decodeAudioData(ev.target.result, function (buffer) {
+                AudioPlayer.createBufferSourse(buffer);
+                AudioPlayer.bufferSource.start(0);
+            });
+        };
+        reader.readAsArrayBuffer(this.files[0]);
     }, false);
-    // TODO: check selecting file
-    function handleFiles() {
-        var fileList = this.files;
-        console.log(fileList);
-        var file = URL.createObjectURL(fileList[0]);
-        audio_player.src = file;
-        audio_player.play();
-    }
+
+    document.getElementById('play').addEventListener('click', function handlePlay() {
+        AudioPlayer.play();
+    }, false);
+
+    document.getElementById('pause').addEventListener('click', function handlePause() {
+        AudioPlayer.pause();
+    }, false);
+
+    document.getElementById('stop').addEventListener('click', function handleStop() {
+        AudioPlayer.stop();
+    }, false);
 })();
